86f74f8856c2d2e3a7d5a26fd462fe9d8cdcd666
andy <andnpatterson@gmail.com>
2019-03-06 15:37:21 -0700

    add setup so can install with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PyExpUtils',
+    url='https://github.com/andnp/PyExpUtils.git',
+    author='Andy Patterson',
+    author_email='andnpatterson@gmail.com',
+    packages=['PyExpUtils'],
+    install_requires=[],
+    version='0.0',
+    license='MIT',
+    description='A small set of utilities for RL and ML experiments',
+    long_description='todo',
+)
