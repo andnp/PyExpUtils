2cd0cc9b0743b2c3acc53da24760f2bba774a14c
andy <andnpatterson@gmail.com>
2019-03-06 16:51:31 -0700

    add travis build setup and git tagging

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -e
+
 export PYTHONPATH=PyExpUtils
 python3 -m unittest discover -p "*test_*.py"
---
+++ scripts/tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# get highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+
+# replace . with space so can split into an array
+VERSION_BITS=(${VERSION//./ })
+
+# get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM2=$((VNUM2+1))
+
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2"
+
+echo "Updating $VERSION to $NEW_TAG"
+
+# get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT`
+
+# only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+    echo "Tagged with $NEW_TAG"
+    # git tag $NEW_TAG
+    # git push --tags
+else
+    echo "Already a tag on this commit"
+fi
