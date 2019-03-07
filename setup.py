fa94aa1489267397a20abd263815805ad585f62f
andy <andnpatterson@gmail.com>
2019-03-06 18:10:09 -0700

    include subpackages in distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PyExpUtils',
     url='https://github.com/andnp/PyExpUtils.git',
     author='Andy Patterson',
     author_email='andnpatterson@gmail.com',
-    packages=['PyExpUtils'],
+    packages=[ p for p in find_packages() if 'tests' not in p ],
     install_requires=[],
     version='0.0',
     license='MIT',
