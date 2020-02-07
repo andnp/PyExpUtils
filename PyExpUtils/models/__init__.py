c55b764f9f813cc0a8840c59abe514bb02b2e104
andy <andnpatterson@gmail.com>
2020-02-07 11:59:11 -0700

    add documentation generator script

--- PyExpUtils/models/__init__.py
+++ PyExpUtils/models/__init__.py
@@ -1,3 +1,6 @@
+"""doc
+A collection of JSON serialization classes with associated utility methods.
+"""
+
 import PyExpUtils.models.Config
 import PyExpUtils.models.ExperimentDescription
-import PyExpUtils.models.RLExperimentDescription
