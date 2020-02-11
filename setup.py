4d42fd81df25e720e333e5d906278f04939b2966
Travis CI User <travis@example.org>
2020-02-11 05:10:33 +0000

    updating to version 1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=[],
-    version=1.0,
+    version=1.1,
     license='MIT',
     description='A small set of utilities for RL and ML experiments',
     long_description='todo',
