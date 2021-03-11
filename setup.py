5bd4699026c9046eb85d26500b5565e59a6d7f2d
github-action <andnpatterson@gmail.com>
2021-03-11 22:21:34 +0000

    updating to version 2.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=['numba'],
-    version=2.8,
+    version=2.9,
     license='MIT',
     description='A small set of utilities for RL and ML experiments',
     long_description='todo',
