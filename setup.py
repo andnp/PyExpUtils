b9d1db47fb89a852903dad5ed938ab5a52596e5d
github-action <andnpatterson@gmail.com>
2022-05-06 02:35:34 +0000

    bump: version 3.2.2 → 3.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="andnpatterson@gmail.com",
     packages=find_packages(exclude=["tests*", "scripts*", "typings*", "mock_repo*"]),
     package_data={"PyExpUtils": ["py.typed"]},
-    version="3.2.2",
+    version="3.2.3",
     license="MIT",
     description="A small set of utilities for RL and ML experiments",
     long_description=long_description,
